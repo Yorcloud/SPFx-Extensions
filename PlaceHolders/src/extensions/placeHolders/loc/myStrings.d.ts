8549c191708eeec3a21b792afa88923c8f7f4ef8
Rob Pearmain <rob@ipear.co.uk>
2021-06-02 10:38:13 +0100

    Added Top and Bottom Placeholder example

---
+++ PlaceHolders/src/extensions/placeHolders/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IPlaceHoldersApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'PlaceHoldersApplicationCustomizerStrings' {
+  const strings: IPlaceHoldersApplicationCustomizerStrings;
+  export = strings;
+}
